a920ddc13dca59357bb679625513e7a2a8857545
gtook101 <78670851+gtook101@users.noreply.github.com>
2021-05-08 17:55:40 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -17,7 +17,7 @@ module.exports = {
   addDepartment: [
     {
       type: "input",
-      name: "department",
+      name: "name",
       message: "What is the department name?",
     },
   ],
@@ -25,7 +25,7 @@ module.exports = {
   addRoles: [
     {
       type: "input",
-      name: "roles",
+      name: "title",
       message: "What is the roles title?",
     },
     {
@@ -35,7 +35,7 @@ module.exports = {
     },
     {
       type: "input",
-      name: "id",
+      name: "department_id",
       message: "What is the roles id?",
     },
   ],
@@ -43,109 +43,23 @@ module.exports = {
   addEmployees: [
     {
       type: "input",
-      name: "firstname",
+      name: "first_name",
       message: "what is the employees first name?",
     },
     {
       type: "input",
-      name: "lastname",
+      name: "last_name",
       message: "What is the employees last name?",
     },
     {
       type: "input",
-      name: "roleid",
+      name: "role_id",
       message: "What is the employees role id?",
     },
     {
       type: "input",
-      name: "managersid",
+      name: "managers_id",
       message: "What is the managers id?",
     },
   ],
-
-  //module.exports = require("managerQuestions");
-
-  // .then((orange => {
-  //   employee.push(
-  //     new this.managerQuestions(
-  //       orange.Name,
-  //       orange.Id,
-  //       orange.Email,
-  //       orange.Office
-  //     )
-  //   );
-  //   console.log(employee);
-  //   managerQuestions();
-  // });
-
-  viewDepartments: () => {
-    console.log("View all Departments...\n");
-    connection.query("SELECT * FROM Departments", (err, res) => {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-      connection.end();
-    });
-  },
-
-  viewRoles: () => {
-    console.log("View all Roles...\n");
-    connection.query("SELECT * FROM roles", (err, res) => {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-      connection.end();
-    });
-  },
-
-  viewEmployees: () => {
-    console.log("View all Employees...\n");
-    connection.query("SELECT * FROM employees", (err, res) => {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-      connection.end();
-    });
-  },
-
-  updateEmployeeRoles: [
-    {
-      //type: "input",
-      //name: "github",
-      //message: "What is the eigneers github?",
-    },
-  ],
-
-  // .then((orange => {
-  //   employee.push(
-  //     new this.managerQuestions(
-  //       orange.Name,
-  //       orange.Id,
-  //       orange.Email,
-  //       orange.GitHub
-  //     )
-  //   );
-  //   console.log(team);
-  //   managerQuestions();
-  // });
-
-  // .then((orange => {
-  //   employee.push(
-  //     new this.managerQuestions(
-  //       orange.Name,
-  //       orange.Id,
-  //       orange.Email,
-  //       orange.Office
-  //     )
-  //   );
-  //   console.log(team);
-  //   managerQuestions();
-  // });
-
-  // endQuestion: {
-  //   type: "list",
-  //   name: "end",
-  //   message: "Would you like to add another employee?",
-  //   choices: ["Yes", "No"],
-  // },
 };
